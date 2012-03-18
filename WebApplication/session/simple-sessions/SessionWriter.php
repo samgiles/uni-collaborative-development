ac6b8a084fe8ba9995e031f4e7d5eff02f658b98
Samuel Giles <aphro.cag@googlemail.com>
2012-03-18 11:57:37 +0000

    Made the function definitions in SessionWriter abstract to ensure they are overridden in their concrete implementation.

--- WebApplication/session/simple-sessions/SessionWriter.php
+++ WebApplication/session/simple-sessions/SessionWriter.php
@@ -4,23 +4,20 @@ abstract class SessionWriter {
    * Reads a session into the session object.
    * Virtual Function, implementation would need to be defined in the concrete super class.
    */
-  public function read($hash) {
-  }
+  abstract public function read($hash);
   
   /**
    * Writes a session object.
-   * Virtual Function, implementation would need to be defined in the concrete super class.
+   * Implementation would need to be defined in the concrete super class.
    */
-  public function write($hash, $sessionObject) {
-  }
+  abstract public function write($hash, $sessionObject);
   
   /**
    * Clears a session.
    * @param $hash The hash/key of the sessions.
    * Virtual Function, implementation would need to be defined in the concrete super class.
    */
-  public function clear($hash) {
-  }
+  abstract public function clear($hash);
   
   /**
    * Persists the session across HTTP requests.  Default is setcookie.
