86feeaca02e89e41d934537271e68a500d938c62
Samuel Giles <aphro.cag@googlemail.com>
2012-03-18 15:28:10 +0000

    Fixed an issue where a variable initialisation was missing in the LoginController

--- WebApplication/core/controllers/LoginController.php
+++ WebApplication/core/controllers/LoginController.php
@@ -9,10 +9,10 @@ class LoginController extends Controller {
  private $_auth;
  private $_redirect = false;
  
- public function __construct($containerController = NULL) {
+  public function __construct($containerController = NULL) {
     $this->_skin = 'default';
     $this->_content = 'login';
-	$this->_layout = 'empty';
+    $this->_layout = 'empty';
     
     $this->_auth = new DBAuthenticate("SYSTEM_USER", "CODE", "USERNAME", "PASSWORD", array());  // Table name: SYSTEM_USER, Primary Key: CODE, Username attribute USERNAME , Password attrbute name PASSWORD. 
      
@@ -29,38 +29,41 @@ class LoginController extends Controller {
     }
      
     if (isset($_GET['go-to'])) {
-         $this->_redirect = $_GET['go-to'];
-    }
- 
-  // Check $_POST variables for uname AND pword.
-  if (isset($_POST['uname']) and isset($_POST['pword'])) {
-    // Do authentication with username and password.
-    $authInfo = $this->tryAuthenticate($_POST['uname'], $_POST['pword']);
-    
-    if ($authInfo !== FALSE) {
-      Session::set('login', $authInfo);
-    }
-    
-  } else {
-    // If no uname and password variable set simply check for current authentication.
-    $loginDetails = Session::get('login');
-
-    // Session Hash doesn't exist if NULL
-    if ($loginDetails != NULL) {
-      if (array_key_exists('ip', $loginDetails) && $loginDetails['ip'] == $_SERVER['REMOTE_ADDR']) {
-         $authInfo = $loginDetails;
-      }
-    } else {
-      $this->_invalidDetails = true;
-    }
-  }
-
-  $this->_hasAuthenticated = ($authInfo !== FALSE);
+      $this->_redirect = $_GET['go-to'];
+    } 
   
-  $this->addViewVariable('invalidDetails', $this->_invalidDetails); 
-  $this->addViewVariable('hasAuthenticated', $this->_hasAuthenticated);
-  $this->addViewVariable('redirect', $this->_redirect);
- }
+    // Initialise variable.
+    $authInfo = false;
+    
+    // Check $_POST variables for uname AND pword.
+    if (isset($_POST['uname']) and isset($_POST['pword'])) {
+      // Do authentication with username and password.
+      $authInfo = $this->tryAuthenticate($_POST['uname'], $_POST['pword']);
+    
+      if ($authInfo !== FALSE) {
+        Session::set('login', $authInfo);
+      }
+    
+    } else {
+      // If no uname and password variable set simply check for current authentication.
+      $loginDetails = Session::get('login');
+
+      // Session Hash doesn't exist if NULL
+      if ($loginDetails != NULL) {
+        if (array_key_exists('ip', $loginDetails) && $loginDetails['ip'] == $_SERVER['REMOTE_ADDR']) {
+          $authInfo = $loginDetails;
+        }
+      } else {
+        $this->_invalidDetails = true;
+      }
+    }
+
+    $this->_hasAuthenticated = ($authInfo !== FALSE);
+  
+    $this->addViewVariable('invalidDetails', $this->_invalidDetails); 
+    $this->addViewVariable('hasAuthenticated', $this->_hasAuthenticated);
+    $this->addViewVariable('redirect', $this->_redirect);
+  }
 
   private function tryAuthenticate($uname, $pword) {
    // Create an Authenticate object.  The Authenticate interface implements a single method: function authenticate($identity (i.e. Username), $credentials (i.e. Password));
