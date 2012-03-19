69590e341d7a0fc0c5c97e783e23b1bab5673607
Samuel Giles <aphro.cag@googlemail.com>
2012-03-19 10:46:12 +0000

    Added a mock payment processor.

---
+++ WebApplication/core/models/PaymentProcessor.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * A simple payment processing service
+ * @author Samuel Giles
+ */
+class PaymentProcessor {
+  
+  /**
+   * Implements a mock payment processing service.
+   * @param array $paymentDetails An associative array containing payment details.
+   */
+  public function authorisePayment(array $paymentDetails) {
+  	return true;
+  }	
+
+}
