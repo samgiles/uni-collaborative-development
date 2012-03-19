1ad01fdd042efeb1ee08aed2085714705b4366a0
v2012 <12345@HOTMAIL.CO.UK>
2012-03-19 12:18:45 +0000

    Updates on error 2

--- WebApplication/core/controllers/RegisterController.php
+++ WebApplication/core/controllers/RegisterController.php
@@ -42,8 +42,8 @@ class RegisterController extends Controller {
       
       // Next we need to add in an address because the SYSTEM_USER table uses a foreign key reference to ADDRESS
       $result = Database::execute("INSERT INTO ADDRESS (NAME, LINE_ONE, LINE_TWO, POST_CODE) VALUES ('', '$addressLineOne', '$addressLineTwo', '$postcode'");
-      
-      $result = $result->fetchAll(PDO::FETCH_ASSOC); 
+      var_dump($result);
+      //$result = $result->fetchAll(PDO::FETCH_ASSOC); 
       
   
       $addressPrimaryKey = $result['CODE']; // Get the primary key of the inserted address to use on the insert into SYSTEM_USER table.
