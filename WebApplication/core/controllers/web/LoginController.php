695e877a3563e81f62f59c2270685125d466b860
Samuel Giles <aphro.cag@googlemail.com>
2012-03-21 07:51:11 +0000

    Refactored LoginController.

--- WebApplication/core/controllers/web/LoginController.php
+++ WebApplication/core/controllers/web/LoginController.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Handles login and logout functionality and manages the session status of authenticated users.
+ * @author Samuel Giles
+ * @package controllers-web
+ */
 class LoginController extends Controller {
     
  // On request with uname and pword POST variables, try and do Database authentication using the DB Auth adapter
@@ -16,22 +21,26 @@ class LoginController extends Controller {
     
     $this->_auth = new DBAuthenticate("SYSTEM_USER", "CODE", "USERNAME", "PASSWORD", array());  // Table name: SYSTEM_USER, Primary Key: CODE, Username attribute USERNAME , Password attrbute name PASSWORD. 
      
-     
-     
-    if (isset($_GET['logout'])) {
-      // clear session.
-      setcookie('logins', null); // removing this seems to break login/logout TODO: find out why
-      Session::clear();
-    }
-     
     if ($containerController !== NULL) {
       $this->addViewVariable('setRedirect', $containerController);   
     }
      
     if (isset($_GET['go-to'])) {
       $this->_redirect = $_GET['go-to'];
-    } 
+    }
+     
+    if (isset($_GET['logout'])) {
+      $this->logout();
+    } else {
+      $this->doLogin();
+    }
+     
+    $this->addViewVariable('invalidDetails', $this->_invalidDetails); 
+    $this->addViewVariable('hasAuthenticated', $this->_hasAuthenticated);
+    $this->addViewVariable('redirect', $this->_redirect);
+  }
   
+  private function doLogin() {
     // Initialise variable.
     $authInfo = false;
     
@@ -57,14 +66,14 @@ class LoginController extends Controller {
         $this->_invalidDetails = true;
       }
     }
-
+    
     $this->_hasAuthenticated = ($authInfo !== FALSE);
-  
-    $this->addViewVariable('invalidDetails', $this->_invalidDetails); 
-    $this->addViewVariable('hasAuthenticated', $this->_hasAuthenticated);
-    $this->addViewVariable('redirect', $this->_redirect);
   }
 
+  private function logout() {
+      Session::clear();
+  }
+  
   private function tryAuthenticate($uname, $pword) {
    // Create an Authenticate object.  The Authenticate interface implements a single method: function authenticate($identity (i.e. Username), $credentials (i.e. Password));
   	return $this->_auth->tryAuthenticate($uname, $pword);
