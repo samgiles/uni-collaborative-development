7da17124deb8566ccba287165f89f4b6e4ce639b
v2012 <12345@HOTMAIL.CO.UK>
2012-03-24 09:24:17 +0000

    COnstant change

--- WebApplication/core/controllers/backoffice/StaffController.php
+++ WebApplication/core/controllers/backoffice/StaffController.php
@@ -6,9 +6,18 @@ class StaffController extends Controller {
 		$this->_layout = 'main';
 		$this->_content = 'Staff';
 		
-		$this->requiredAccess(AccessLevels::STAFF | AccessLevels::SUPERVISOR | AccessLevels::ADMIN);
+		$this->requiredAccess(AccessLevels::GENERALSTAFF | AccessLevels::SUPERVISOR | AccessLevels::ADMIN);
         $this->getAllStaff();
         // Tell the view that we're an Index controller.
 		$this->addViewVariable("c", "Staff");
+        
+    } 
+	
+	private function getAllStaff() {
+	  $sqlStatement = 'SELECT  `STAFF`.`CODE`, `NAME`,`ADDRESS`.`LINE_ONE`, `ADDRESS`.`LINE_TWO`, `ADDRESS`.`POST_CODE` FROM WHOLESALER, ADDRESS WHERE `WHOLESALER`.`ADDRESS_CODE` = `ADDRESS`.`CODE`';
+	  $result = Database::execute($sqlStatement);
+	  $result = $result->fetchAll();
+	  
+	  $this->addViewVariable('Staff', $result);
 	}
 }
