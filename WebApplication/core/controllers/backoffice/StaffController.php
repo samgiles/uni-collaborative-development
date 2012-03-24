699262b5ddf4b33afda90bb25de41798b29fc250
v2012 <12345@HOTMAIL.CO.UK>
2012-03-24 08:50:19 +0000

    Updateded class

--- WebApplication/core/controllers/backoffice/StaffController.php
+++ WebApplication/core/controllers/backoffice/StaffController.php
@@ -1,14 +1,14 @@
 <?php
-class WholesalerController extends Controller {
+class StaffController extends Controller {
     
     public function __construct() {
     	$this->_skin = 'default';
 		$this->_layout = 'main';
-		$this->_content = 'wholesalers';
+		$this->_content = 'Staff';
 		
-		$this->requiredAccess(AccessLevels::WAREHOUSE | AccessLevels::SUPERVISOR | AccessLevels::ADMIN);
-        $this->getAllWholesalers();
+		$this->requiredAccess(AccessLevels::STAFF | AccessLevels::SUPERVISOR | AccessLevels::ADMIN);
+        $this->getAllStaff();
         // Tell the view that we're an Index controller.
-		$this->addViewVariable("c", "Wholesaler");
+		$this->addViewVariable("c", "Staff");
 	}
 }
