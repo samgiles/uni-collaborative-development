05ff583f6fe9ed8b5298769b33003e453c536154
aPhRo <aphro.cag@googlemail.com>
2012-03-25 12:22:38 +0100

    Removed unused parameter.

--- WebApplication/core/PageModel.php
+++ WebApplication/core/PageModel.php
@@ -12,7 +12,7 @@ class PageModel {
 	 * Magic method used to retrieve a variable from the underlying hash map.  If the value does not exist NULL will be returned.
 	 * @see http://uk.php.net/manual/en/language.oop5.overloading.php#object.call for more information on magic methods.
 	 */
-	public function __call($name, $args) {
+	public function __call($name) {
 		if (array_key_exists($name, $this->_pageVariables)){
 			return $this->_pageVariables[$name];
 		}
