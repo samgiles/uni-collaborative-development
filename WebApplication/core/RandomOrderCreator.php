e604955d264487dcbc77399d11305dfd6f9f165d
aPhRo <aphro.cag@googlemail.com>
2012-03-25 15:28:11 +0100

    Updated the random order creator script.. this is slow and lazy, but it's only run once or twice..

--- WebApplication/core/RandomOrderCreator.php
+++ WebApplication/core/RandomOrderCreator.php
@@ -36,7 +36,7 @@ $timeNow = time();
 for ($i = 0; $i < 100; ++$i) {
 	$newTime = floor(rand(0, 640000));
 	
-	$sql = "INSERT INTO PURCHASE_INVOICE (DATE, CUSTOMER_CODE, PAYMENT_RECEIVED) VALUES (" . ($timeNow - $newTime) . ", 1, 1)";
+	$sql = "INSERT INTO PURCHASE_INVOICE (DATE, CUSTOMER_CODE, PAYMENT_RECEIVED, POSTAGE_PRICE) VALUES (" . ($timeNow - $newTime) . ", 1, 1, 3.99)";
 	$result = Database::execute($sql);
 	$result = Database::execute("SELECT LAST_INSERT_ID() as CODE"); // Get the ID of the last inserted record.
 	$result = $result->fetch(PDO::FETCH_ASSOC);
@@ -46,8 +46,14 @@ for ($i = 0; $i < 100; ++$i) {
 	while ((rand(0, 10) < 8 && $a < 15) || $a < 1) {
 		$a++;
 		$quantity = rand(1, 6);
-		$product = floor(rand(1, 4));
-		$sql = "INSERT INTO PURCHASE_INVOICE_PRODUCT (PURCHASE_INVOICE_CODE, PRODUCT_CODE, QUANTITY) VALUES ({$primaryKey}, $product, $quantity)";
+		$product = floor(rand(2, 10));
+		$sql = "SELECT UNIT_PRICE FROM PRODUCT WHERE CODE=$product";
+		$result = Database::execute($sql);
+		
+		$price = $result->fetchAll();
+		$price = $price[0]['UNIT_PRICE'];
+		
+		$sql = "INSERT INTO PURCHASE_INVOICE_PRODUCT (PURCHASE_INVOICE_CODE, PRODUCT_CODE, QUANTITY, UNIT_PRICE) VALUES ({$primaryKey}, $product, $quantity, $price)";
 		Database::execute($sql);
 	}
 }
