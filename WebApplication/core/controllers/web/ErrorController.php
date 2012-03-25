560a6648c2180c063aad55c82f625d16f237d448
aPhRo <aphro.cag@googlemail.com>
2012-03-25 06:29:42 +0100

    Added a simple 404 page.

--- WebApplication/core/controllers/web/ErrorController.php
+++ WebApplication/core/controllers/web/ErrorController.php
@@ -1,4 +1,12 @@
 <?php
 class ErrorController extends Controller {
+	public function __construct() {
+		$this->_skin = 'default';
+		$this->_layout = 'main';
+		$this->_content = 'error.404';
 	
+	
+		// Tell the view that we're an Index controller.
+		$this->addViewVariable("c", "Error");
+	}
 }
