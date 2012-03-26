341b8c035c3534c7dc984b00afddaaccd3d91f0f
v2012 <12345@HOTMAIL.CO.UK>
2012-03-26 02:49:04 +0000

    New Content created

---
+++ WebApplication/core/controllers/backoffice/WarehouseController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Provides the interface for viewing the list of Warehouse.
+ * @author Vishal Patel
+ * @package application-controller
+ * @version 0.2
+ */
+class WarehouseController extends Controller {
+    
+    public function __construct() {
+    	$this->_skin = 'default';
+		$this->_layout = 'main';
+		$this->_content = 'Warehouse';
+		
+		$this->requiredAccess(AccessLevels::WAREHOUSE | AccessLevels::SUPERVISOR | AccessLevels::ADMIN);
+        $this->getAllWarehouse();
+        // Tell the view that we're an Index controller.
+		$this->addViewVariable("c", "Warehouse");
+	}
+	
+	
+	private function getAllWarehouse() {
+	  $sqlStatement =
+	  $result = Database::execute($sqlStatement);
+	  $result = $result->fetchAll();
+	  
+	  $this->addViewVariable('Warehouse', $result);
+	}
+}
