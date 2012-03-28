c7eacce413446f25d45a737999395bb23d5e0c5b
aPhRo <aphro.cag@googlemail.com>
2012-03-28 16:42:12 +0100

    Docs.

--- WebApplication/core/com/Observable.php
+++ WebApplication/core/com/Observable.php
@@ -1,19 +1,44 @@
 <?php
+/**
+ * Defines an observable class, this can be oberved by Observer's.
+ * @author Samuel Giles
+ * @package core-com
+ * @subpackage core
+ * @version 0.2
+ */
 abstract class Observable {
 	
+	/**
+	 * Maintains a list of registered Observers
+	 * @var SplObjectStorage (Should only be storing objects that implement Observer)
+	 */
 	protected $_observers;
 	
+	/**
+	 * Creates a new Observable and sets up the underlying storage mechanism.
+	 */
 	public function __construct() {
 		$this->_observers = new SplObjectStorage();
 	}
 	
+	/**
+	 * Attach an observer to this observable to be notified of updates.
+	 * @param Observer $observer The observer to attach
+	 */
 	public function attach(Observer $observer) {
 		$this->_observers->attach($observer);
 	}
 	
+	/**
+	 * Detach an observer from this observable so it no longer is notified of updates.
+	 * @param Observer $observer The observer to detach.
+	 */
 	public function detach (Observer $observer) {
 		$this->_observers->detach($observer);
 	}
 	
+	/**
+	 * Notify notifies listeners of the updated Observable.
+	 */
 	abstract public function notify();
 }
--- WebApplication/core/com/Observer.php
+++ WebApplication/core/com/Observer.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Provides an abstract interface for an Observer object.  To be used when implementing an Observer pattern.
+ * @author Samuel Giles
+ * @package core-com
+ * @subpackage core
+ * @version 0.2
+ */
 abstract class Observer {
 	abstract public function update (Observable $subject);
 }
