7bc738a6c2f746d3e79b4a6842107ddf57c6b2af
aPhRo <aphro.cag@googlemail.com>
2012-03-30 20:02:16 +0100

    Docs.

--- WebApplication/session/simple-sessions/SessionWriter.php
+++ WebApplication/session/simple-sessions/SessionWriter.php
@@ -1,36 +1,54 @@
 <?php
+/**
+ * Provides an interface to a for reading and writing session data.
+ * @author Samuel Giles
+ * @package core
+ * @subpackage core-sessions
+ * @version 1.0
+ */
 abstract class SessionWriter {
   /**
    * Reads a session into the session object.
    * Virtual Function, implementation would need to be defined in the concrete super class.
+   * @param string $hash The hash to read.
    */
   abstract public function read($hash);
   
   /**
    * Writes a session object.
    * Implementation would need to be defined in the concrete super class.
+   * @param string $hash The value to write.
+   * @param array $sessionObject The value to write.
    */
   abstract public function write($hash, $sessionObject);
   
   /**
    * Clears a session.
    * @param $hash The hash/key of the sessions.
-   * Virtual Function, implementation would need to be defined in the concrete super class.
    */
   abstract public function clear($hash);
   
   /**
    * Persists the session across HTTP requests.  Default is setcookie.
+   * @param $keyIdentifier  The value that will dientify the session between http requests.
    */
   public function httpPersist($keyIdentifier, $sessionHash, $sessionExpires, $sessionPath) {
     setcookie($keyIdentifier, $sessionHash, $sessionExpires, $sessionPath === -1 ? $_SERVER['PATH_INFO'] : $sessionPath);
   }
   
+  /**
+   * Clears the http persistance.
+   * @param string $keyIdentifier
+   */
   public function httpClear($keyIdentifier) {
     unset($_COOKIE[$keyIdentifier]);
     setcookie($keyIdentifier, NULL, -1);
   }
   
+  /**
+   * Gets the key that was persisted.
+   * @param string $keyIdentifier
+   */
   public function getHttpPersistKey($keyIdentifier) {
       if (isset($_COOKIE[$keyIdentifier])) {
   	  return $_COOKIE[$keyIdentifier];
--- WebApplication/session/simple-sessions/session_writers/PDOSessionWriter.php
+++ WebApplication/session/simple-sessions/session_writers/PDOSessionWriter.php
@@ -1,70 +1,115 @@
 <?php
+/**
+ * An implementation of the session writer that writes the session data to a PDO data source.
+ * @author Samuel Giles
+ * @package core
+ * @subpackage core-sessions
+ */
 class PDOSessionWriter extends SessionWriter {
+	
+  	/**
+  	 * The PDO object used to connect to the database.
+  	 * @var PDO
+  	 */
+  	private $_pdoObject;
+  	
+  	/**
+  	 * The PDO statement for reading the datasource.
+  	 * @var PDOStatement
+  	 */
+ 	private $_pdoReadStatement = NULL;
+ 	
+ 	/**
+ 	 * The PDO statement used for writing the datasource
+ 	 * @var PDOStatement
+ 	 */
+  	private $_pdoWriteStatement = NULL;
+  	
+  	/**
+  	 * The PDO statement used for deleting from the datasource.
+  	 * @var PDOStatement 
+  	 */
+ 	private $_pdoDeleteStatement = NULL;
   
-  private $_pdoObject;
-  private $_pdoReadStatement = NULL;
-  private $_pdoWriteStatement = NULL;
-  private $_pdoDeleteStatement = NULL;
+ 	/**
+ 	 * Determines whether we're in the database yet or not.
+ 	 * @var boolean
+ 	 */
+  	private $_inDatabase = false;
   
-  private $_inDatabase = false;
-  
-  public function __construct(PDO $pdoObject) {
-    $this->_pdoObject = $pdoObject;
-    $this->_pdoReadStatement = NULL;
-    $this->_pdoWriteStatement = NULL;
-    $this->_pdoDeleteStatement = null;
-  }
-
-  public function read($hash) {
-      if ($this->_pdoReadStatement === NULL || !isset($this->_pdoReadStatement)) {
-  	  $this->_pdoReadStatement = $this->_pdoObject->prepare("SELECT `SESSION`.`VALUE` FROM `SESSION` WHERE `SESSION`.`KEY`=:hash LIMIT 1;");
+  	/**
+  	 * Creates a new PDOSessionWriter with the given PDO object.
+  	 * @param PDO $pdoObject
+  	 */
+ 	public function __construct(PDO $pdoObject) {
+    	$this->_pdoObject = $pdoObject;
+    	$this->_pdoReadStatement = NULL;
+    	$this->_pdoWriteStatement = NULL;
+    	$this->_pdoDeleteStatement = null;
   	}
-    $this->_pdoReadStatement->bindValue(':hash', $hash, PDO::PARAM_STR);
-    $this->_pdoReadStatement->execute();
-    $row = $this->_pdoReadStatement->fetch(PDO::FETCH_ASSOC);
-    
-    // Below we're setting a flag so we know whether to make an INSERT or UPDATE SQL statement when we write the session back in.
-    if (!$row) {
-      $this->_inDatabase = false;
-      return NULL;
-    }
-    
-    $this->_inDatabase = true;
-    $object = unserialize($row['VALUE']);
-    return $object;
-  }
 
-  public function write($hash, $sessionObject) {
-    if ($this->_pdoWriteStatement === NULL) {
-      if ($this->_inDatabase) {
-        $this->_pdoWriteStatement = $this->_pdoObject->prepare("UPDATE `SESSION` `SESSION`.VALUE`=:object WHERE `SESSION`.`KEY`=:hash");
-      } else {
-        $this->_pdoWriteStatement = $this->_pdoObject->prepare("INSERT INTO SESSION VALUES (:hash, :object)");
-      }
-    }
+  	/**
+  	 * (non-PHPdoc)
+  	 * @see SessionWriter::read()
+  	 */
+  	public function read($hash) {
+   	  	if ($this->_pdoReadStatement === NULL || !isset($this->_pdoReadStatement)) {
+  	  		$this->_pdoReadStatement = $this->_pdoObject->prepare("SELECT `SESSION`.`VALUE` FROM `SESSION` WHERE `SESSION`.`KEY`=:hash LIMIT 1;");
+  		}
+    	$this->_pdoReadStatement->bindValue(':hash', $hash, PDO::PARAM_STR);
+    	$this->_pdoReadStatement->execute();
+    	$row = $this->_pdoReadStatement->fetch(PDO::FETCH_ASSOC);
     
-    $this->_pdoWriteStatement->bindValue(':hash', $hash, PDO::PARAM_STR);
+    	// Below we're setting a flag so we know whether to make an INSERT or UPDATE SQL statement when we write the session back in.
+   	 	if (!$row) {
+      		$this->_inDatabase = false;
+      		return NULL;
+    	}
     
-    $object = serialize($sessionObject);
-    $this->_pdoWriteStatement->bindValue(':object', $object, PDO::PARAM_LOB);
+    	$this->_inDatabase = true;
+    	$object = unserialize($row['VALUE']);
+    	return $object;
+ 	}
+	
+ 	/**
+ 	 * (non-PHPdoc)
+ 	 * @see SessionWriter::write()
+ 	 */
+  	public function write($hash, $sessionObject) {
+   	 	if ($this->_pdoWriteStatement === NULL) {
+      		if ($this->_inDatabase) {
+        		$this->_pdoWriteStatement = $this->_pdoObject->prepare("UPDATE `SESSION` `SESSION`.VALUE`=:object WHERE `SESSION`.`KEY`=:hash");
+      		} else {
+        		$this->_pdoWriteStatement = $this->_pdoObject->prepare("INSERT INTO SESSION VALUES (:hash, :object)");
+      		}
+    	}
     
-    $this->_pdoWriteStatement->execute();
-    $this->_inDatabase = true;
+    	$this->_pdoWriteStatement->bindValue(':hash', $hash, PDO::PARAM_STR);
     
-    return true; // Tidy up return interfaces.. We dont NEED to return anything but should probably tidy them up.
-  }
+    	$object = serialize($sessionObject);
+    	$this->_pdoWriteStatement->bindValue(':object', $object, PDO::PARAM_LOB);
+    
+    	$this->_pdoWriteStatement->execute();
+    	$this->_inDatabase = true;
+    
+    	return true; // Tidy up return interfaces.. We dont NEED to return anything but should probably tidy them up.
+  	}
   
-  public function clear($hash) {
-    if ($this->_pdoDeleteStatement === NULL) {
-      $this->_pdoDeleteStatement = $this->_pdoObject->prepare("DELETE FROM `SESSION` WHERE `SESSION`.`KEY`=:hash");
-    }
+  	/**
+  	 * (non-PHPdoc)
+  	 * @see SessionWriter::clear()
+  	 */
+  	public function clear($hash) {
+    	if ($this->_pdoDeleteStatement === NULL) {
+      		$this->_pdoDeleteStatement = $this->_pdoObject->prepare("DELETE FROM `SESSION` WHERE `SESSION`.`KEY`=:hash");
+    	}
     
-    if ($this->_inDatabase) {
-      $this->_pdoDeleteStatement->bindValue(':hash', $hash, PDO::PARAM_STR);
-      $this->_pdoDeleteStatement->execute();
-      return true;
-    } else {
-      return false;
-    }
-  }
+    	if ($this->_inDatabase) {
+      		$this->_pdoDeleteStatement->bindValue(':hash', $hash, PDO::PARAM_STR);
+      		$this->_pdoDeleteStatement->execute();
+      		return true;
+    	} else {
+      		return false;
+    	}
+  	}
 }
